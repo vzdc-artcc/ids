56cf3efb77add5919c39c26531222b10f10d2002
Aneesh Reddy <aneeshjreddy@gmail.com>
2025-08-01 14:37:28 -0400

    Add real-time SUA request activation and deactivation with socket integration

--- components/SuaRequest/SuaRequestInformation.tsx
+++ components/SuaRequest/SuaRequestInformation.tsx
@@ -1,8 +1,10 @@
 'use client';
 import React, {useEffect, useState} from 'react';
-import {Box, Divider, Grid2, Typography} from "@mui/material";
+import {Box, CircularProgress, Divider, Grid2, Switch, Tooltip, Typography} from "@mui/material";
 import {fetchSuaRequests} from "@/actions/sua";
+import {socket} from "@/lib/socket";
 import {formatZuluDate} from "@/lib/date";
+import {toast} from "react-toastify";
 
 export default function SuaRequestInformation() {
 
@@ -23,46 +25,58 @@ export default function SuaRequestInformation() {
             topAltitude: number;
         }[];
     }[]>();
+    const [activeSuas, setActiveSua] = useState<string[]>([]);
 
     useEffect(() => {
         const intervalId = setInterval(() => {
             fetchSuaRequests().then(setSuaRequests);
-        }, 60000);
+        }, 5000);
+
+        socket.on("sua-activate", (id: string) => {
+            setActiveSua((prev) => [...prev, id]);
+            toast.info('SUA requests updated.');
+        });
+
+        socket.on("sua-deactivate", (id: string) => {
+            setActiveSua((prev) => prev.filter((suaId) => suaId !== id));
+            toast.info('SUA requests updated.');
+        });
 
         return () => {
             clearInterval(intervalId);
+
+            socket.off("sua-activate");
+            socket.off("sua-deactivate");
         };
     });
 
-    const getSuaRequestStatus = (request: {
-        start: Date;
-        end: Date;
-    }): { color: string, text: string, } => {
-        const now = new Date();
-        if (now < new Date(request.start)) {
-            if (new Date(request.start).getTime() - now.getTime() <= 15 * 60 * 1000) {
-                return {color: 'cyan', text: 'STARTING'};
-            }
-            return {color: 'gold', text: 'SCHEDULED'};
-        } else if (now >= new Date(request.start) && now <= new Date(request.end)) {
-            if (new Date(request.end).getTime() - now.getTime() <= 15 * 60 * 1000) {
-                return {color: 'orange', text: 'ENDING'};
-            }
-            return {color: 'lightgreen', text: 'ACTIVE'};
-        } else {
-            return {color: 'white', text: 'EXPIRED'};
+    const toggleActiveSuaRequest = (id: string) => {
+        if (activeSuas.includes(id)) {
+            socket.emit("sua-deactivate", id);
+            return;
         }
+        socket.emit("sua-activate", id);
     }
 
     return (
         <Grid2 size={5} sx={{border: 1, overflowY: 'auto',}}>
-            <Typography variant="h6">SUA (1m)</Typography>
+            <Typography variant="h6">SUA</Typography>
             <Box height={250} sx={{overflow: 'auto',}}>
+                {!suaRequests && <CircularProgress/>}
+                {!suaRequests && <Typography>Loading will take at least 1 minute.</Typography>}
                 <Grid2 container columns={2} spacing={1}>
                     {suaRequests && suaRequests.map((request) => (
                         <Grid2 size={{xs: 2, lg: 1}} key={request.id} sx={{p: 0.5, border: 1, borderColor: 'red',}}>
                             <Typography variant="h6" fontSize={16}
-                                        color={getSuaRequestStatus(request).color}>#{request.missionNumber} {getSuaRequestStatus(request).text}</Typography>
+                                        color={activeSuas.includes(request.id) ? 'lightgreen' : 'gold'}>#{request.missionNumber} {activeSuas.includes(request.id) ? 'ACTIVE' : (new Date(request.start) > new Date() ? 'SCHEDULED' : 'INACTIVE')}</Typography>
+                            <Tooltip title="Toggle Active">
+                                <Switch
+                                    checked={activeSuas.includes(request.id)}
+                                    onChange={() => toggleActiveSuaRequest(request.id)}
+                                    size="small"
+                                    color="info"
+                                />
+                            </Tooltip>
                             <Typography variant="body1">
                                 {formatZuluDate(request.start)} - {formatZuluDate(request.end)}
                             </Typography>
