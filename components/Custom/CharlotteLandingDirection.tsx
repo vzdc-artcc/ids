4192324b535f7d963dce015e65c0bb2fd8e5d707
Aneesh Reddy <aneeshjreddy@gmail.com>
2026-07-06 22:04:15 -0400

    Removed unused import.

--- components/Custom/CharlotteLandingDirection.tsx
+++ components/Custom/CharlotteLandingDirection.tsx
@@ -1,5 +1,5 @@
 'use client';
-import React, {useEffect, useState} from 'react';
+import React, {useState} from 'react';
 import {Button} from "@mui/material";
 import {toggleCltLandingDirection} from "@/actions/custom";
 import {toast} from "react-toastify";
